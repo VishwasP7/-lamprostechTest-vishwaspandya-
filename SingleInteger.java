5c2935aa938aa19a45771e9762700fbe201236b6
VishwasP7 <122794474+VishwasP7@users.noreply.github.com>
2023-01-16 18:26:52 +0530

    Update SingleInteger.java

--- SingleInteger.java
+++ SingleInteger.java
@@ -1,1 +1,35 @@
 
+import java.io.*;
+
+public class singleInteger
+{
+public static int findSingleInt(int A[], int arrSize)
+{
+
+for (int i = 0; i < arrSize; i++)
+{
+int count = 0;
+for (int j = 0; j < arrSize; j++)
+{
+if (A[i] == A[j])
+{
+count++;
+}
+}
+if (count == 1)
+{
+return A[i];
+}
+}
+
+return -1;
+}
+
+public static void main (String[] args)
+{
+int arr[] = {4,1,2,1,2};
+int n = arr.length;
+
+System.out.println("Element occurring once is " + findSingleInt(ar, n));
+}
+}
